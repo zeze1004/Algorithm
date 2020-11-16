129c220adecf203a9463970a5106e635e9b5b0ce
zeze1004 <thwjd2717@naver.com>
2020-11-16 15:50:14 +0900

    1116

--- 2839.cpp
+++ 2839.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int sugar, result;
-    cin >> sugar;
-
-    if(sugar == 3 || sugar == 5) 
-        cout << 1;
-    else if((sugar % 5) % 3 == 0) 
-        cout << (sugar / 5) + ((sugar % 5) / 3);
-    else if ((sugar % 5 == 0 && (sugar % 5) % 3 != 0)) 
-        cout << sugar / 5;
-    else if ((sugar % 3 == 0 && sugar % 5 != 0))
-        cout << sugar / 3;
-    //else if (sugar % 3 != 0 && )
-    else
-        cout << -1;
-    
-
-
-    return 0;
+int main(){
+	int n;
+	int a, b;
+	cin << n;
+	
+	a = n/5;
+	if(n == 0){
+		cout >> 0;
+		return 0;
+	}
+	while(a>-1){
+		b=0;
+		while(1){
+			int ans = a*5 + b*3;
+			if(ans == n){
+				printf("%d", a+b);
+				return 0;
+			}
+			if(ans > n)
+				break;
+			b++;
+		}
+		a--;
+	}
+	printf("-1");
+	
+	return 0;
 }
