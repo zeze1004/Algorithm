c7440d1b7fbf57ac0e35e4c45db452bac14f3d24
zeze1004 <thwjd2717@naver.com>
2020-11-28 06:04:58 +0900

    시립대 알고리즘 예비소집 2번 문제

--- 시립대_알고리즘_대회/palindrome.cpp
+++ 시립대_알고리즘_대회/palindrome.cpp
@@ -7,19 +7,35 @@ int main() {
     ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int num, input;
-    vector <int> v;
-    cin >> num;
-    for(int i = 0; i < num; i++) {
-        cin >> input;
-        v.push_back(input);
-        int tmp = input;
-        if(v[i+1] > tmp) {
-            
-        }
+    
+    int n;
+    int arr[1000];
+    cin >> n;
+    for(int i = 0; i < n; ++i)
+        cin >> arr[i];
+    
+    bool isPalin = true;
+    for(int i = 0; i < n/2 && isPalin; ++i) {
+        if(arr[i] != arr[n-1-i])
+            isPalin = false;
     }
-
+    if(isPalin) {
+        bool increasing = arr[0] < arr[1];
+        bool isMixed = arr[0] == arr[1];
+        for(int i = 1; i < (n%2 == 0 ? n/2-1 : n/2) && !isMixed; ++i) {
+            if(increasing && arr[i] >= arr[i+1] ||
+                !increasing && arr[i] <= arr[i+1]
+            )
+                isMixed = true;
+        }
+            if(isMixed)
+                cout << 3;
+            else if(increasing)
+                cout << 1;
+            else
+                cout << 2; 
+    } else
+        cout << "D";
 
     return 0;
 }
