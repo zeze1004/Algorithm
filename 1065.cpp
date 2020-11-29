054a0bfdaf92d9a1fb95d11e13719a4334d856e1
zeze1004 <thwjd2717@naver.com>
2020-11-29 12:51:20 +0900

    해결

--- 1065.cpp
+++ 1065.cpp
@@ -1,43 +1,33 @@
-
-
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int hansu(int n) {
-
-    int count = 0;
-    int arr[3];
-
-    for(int i = 100; i <= n; i++) {
-        arr[0] = i / 100;
-        cout << arr[0];
-        arr[1] = (i / 100) / 10;
-        cout << arr[1];
-        arr[2] = i % 10;
-        cout << arr[2];
-        if((arr[0]+arr[2])==arr[1]*2) {
-            count ++;
-            cout << "why??" << '\n';
+    int count = 99; // 한수면 ++
+    for(int i = 111; i <= n; i++) {
+        int first = i / 100; // 100의 자리
+        int second = i / 10 - first*10; // 10의 자리
+        int third = i - first*100 - second*10; // 1의 자리
+        if(first-second == second - third) { // 연속된 자리의 수가 등차수열인지 확인
+            count++;
         }
     }
-    return count+99;
+    return count;
 }
-
 
 int main() {
     ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
     int n;
     cin >> n;
-    if(n >= 100) {
-        int count = hansu(n);
-        cout << count;
-    } 
-    else {
-        cout << n;
-    }
-
+    if (n <= 110) {
+        if( n < 100) {
+            cout << n;
+            return 0;
+        } else cout << 99;
     return 0;
+    } else {
+        cout << hansu(n);
+    }
 }
