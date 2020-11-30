26cec5105089198462c1ca99dd07365fa1c62b65
zeze1004 <thwjd2717@naver.com>
2020-11-30 14:35:39 +0900

    드디어 해결!

--- 10809.cpp
+++ 10809.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string.h>
+#include <cstring>
 #include <vector>
 using namespace std;
 #define N 100
@@ -8,16 +8,29 @@ int main() {
     ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cout << "a";
-    
-    char *input, compare[N];
-    for(char i = 'a'; i <= 'z'; i++) {
-        compare[i] = i;
+        
+    char *input1;
+    int compare[N];
+    string input2;
+    for(int i = 0; i < 26; i++) {
+        compare[i] = -1;
     }
+
     // 입력
-    // cin >> input;
-    
+    getline (cin, input2);
 
-
+    for(int i = 0; i < 26; i++) {
+        for(int j = 0; j < input2.size(); j++) {
+            if((int)input2[j] - 97 == i) {
+                // 중복체크
+                if(compare[i] != -1) {
+                    continue;
+                } else compare[i] = j;  
+            }
+        }
+    }
+    for(int i = 0; i < 26; i++) {
+        cout << compare[i] << " ";
+    }
     return 0;
 }
