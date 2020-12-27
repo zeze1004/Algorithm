549f8b7a59b1f47a535e6a3c7ec4ccde54a59284
zeze1004 <thwjd2717@naver.com>
2020-12-27 15:50:10 +0900

    1094 해결

--- 1094.cpp
+++ 1094.cpp
@@ -3,18 +3,21 @@
 using namespace std;
 int main()
 {
-  cin.tie(NULL);
-  int x;
-  cin >> x;
-  int num = 0;
-  while (x != 0)
-  {
-    if (x % 2 == 1)
-    {
-      num++;
-    }
-    x = x / 2;
-    // cout << "x " << x << '\n';
-  }
-  cout << num;
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x;
+	cin >> x;
+	int num = 0;
+	while (x != 0)
+	{
+		if (x % 2 == 1)
+		{
+			num++;
+		}
+		x = x / 2;
+		// cout << "x " << x << '\n';
+	}
+	cout << num;
 }
