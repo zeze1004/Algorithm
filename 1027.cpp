896e3a477ca95bf026d2547e66c8bd25a236c85f
zeze1004 <thwjd2717@naver.com>
2020-12-31 16:28:52 +0900

    1027 문제 풀이 중

---
+++ 1027.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int building[51];
+
+double slope(int near, int center)
+{
+	return ((building[near] - building[center]) / (near - center));
+}
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	double Mslope1, Mslope2, Pslope1, Pslope2;
+	int i, j, k;
+	int num[51] = {0};
+	cin >> N;
+
+	for (i = 1; i <= N; i++)
+	{
+		cin >> building[i];
+	}
+
+	for (i = 1; i <= N; i++)
+	{
+		if (i > 1)
+		{
+			num[i]++;
+			Mslope1 = slope(i - 1, i);
+			for (j = i - 2; j >= 1; j--)
+			{
+				Mslope2 = slope(j, i);
+				if (Mslope1 > Mslope2)
+				{
+					Mslope1 = Mslope2;
+					num[i]++;
+				}
+			}
+		}
+
+		if (i < N)
+		{
+			num[i]++;
+			Pslope1 = slope(i + 1, i);
+			for (k = i + 2; k <= N; k++)
+			{
+				Pslope2 = slope(k, i);
+				if (Pslope1 < Pslope2)
+				{
+					Pslope1 = Pslope2;
+					num[i]++;
+				}
+			}
+		}
+		cout << i << " " << num[i] << '\n';
+	}
+	int max = num[1];
+	for (i = 2; i <= N; i++)
+	{
+		if (max < num[i])
+		{
+			max = num[i];
+		}
+	}
+	cout << max << '\n';
+}
