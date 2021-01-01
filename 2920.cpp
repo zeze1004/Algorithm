e00de0d5694e345c58849d0187792fd9e6f0d3b4
zeze1004 <thwjd2717@naver.com>
2021-01-01 21:39:45 +0900

    class 1 인장 획득

--- 2920.cpp
+++ 2920.cpp
@@ -3,4 +3,32 @@ using namespace std;
 
 int main()
 {
+	bool ascced = true, desced = true;
+	int prev, next;
+	cin >> prev;
+	for (int i = 0; i < 7; i++)
+	{
+		cin >> next;
+		if (prev < next)
+		{
+			desced = false;
+		}
+		else if (prev > next)
+		{
+			ascced = false;
+		}
+		prev = next;
+	}
+	if (ascced)
+	{
+		cout << "ascending";
+	}
+	else if (desced)
+	{
+		cout << "descending";
+	}
+	else
+	{
+		cout << "mixed";
+	}
 }
