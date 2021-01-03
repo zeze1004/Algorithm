90263805fd6ff1c45df58e5729037aa9cd3a4181
zeze1004 <thwjd2717@naver.com>
2021-01-04 00:02:45 +0900

    실패...

--- 1018.cpp
+++ 1018.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-main()
+int main()
 {
-	int x, , y, i = 0, j = 0;
-	cin >> x >> y;
-	char chess[50][50];
-	for (i; i < x; i++)
+	char chess[250];
+	int y, x, w_num = 0, b_num = 0, i = 0, j = 0;
+	cin >> y >> x;
+	for (int i = 0; i < x * y; i++)
 	{
-		for (j; j < y; j++)
+		cin >> chess[i];
+	}
+
+	// 시작이 w인 체스판
+	for (i; i < x * y; i++)
+	{
+		for (j = i; j < 64 + i; j++)
 		{
-			cin >> chess[i][j];
-			if (chess[i][j])
+			if (j - 8)
+			{
+				if (chess[j] != 'B' && chess[j] != 'W')
+					break;
+				if (i % 2 == 0) // i가 짝수일 때
+				{
+					if (k % 2 == 0)
+					{
+						if (chess[i] != 'W')
+						{
+							w_num++;
+						}
+					}
+					else
+					{
+						if (chess[i] != 'B')
+						{
+							w_num++;
+						}
+					}
+				}
+			}
 		}
 	}
+	cout << w_num;
 	return 0;
 }
