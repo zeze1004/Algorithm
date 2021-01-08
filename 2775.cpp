e87d1786ec01c9b47d1e383ce0a78e7507beb555
zeze1004 <thwjd2717@naver.com>
2021-01-09 01:29:28 +0900

    2775 해결

--- 2775.cpp
+++ 2775.cpp
@@ -3,18 +3,29 @@ using namespace std;
 
 int main()
 {
-	int T, k, n, sum = 0;
-	cin >> T;
-	for (int i = 0; i < T; i++)
+	int T, k, n, apart[20][20] = {0}; // apart[층][호]
+	for (int i = 0; i <= 14; i++)			// 층
 	{
-		cin >> k >> n;
-		for (int j = 0; j <= k; j++)
+		for (int j = 1; j <= 14; j++) // 호
 		{
-			for (int q = 1; q <= n; q++)
+			apart[i][1] = 1; // 몇 층이든 1호는 무조건 1명 거주
+			if (i == 0)
 			{
-				sum
+				apart[i][j] = j;
+				// cout << i << " " << j << " " << apart[i][j] << '\n';
+			}
+			else if (i > 0)
+			{
+				apart[i][j] = apart[i - 1][j] + apart[i][j - 1];
+				// cout << i << " " << j << " " << apart[i][j] << '\n';
 			}
 		}
+	}
+	cin >> T;
+	while (T--)
+	{
+		cin >> k >> n;
+		cout << apart[k][n] << '\n';
 	}
 
 	return 0;
