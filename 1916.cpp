caa011bf07dcce92458317eff4fc7444bafe3567
zeze1004 <thwjd2717@naver.com>
2021-01-10 14:19:59 +0900

    1916

--- 1916.cpp
+++ 1916.cpp
@@ -6,49 +6,53 @@
 
 using namespace std;
 
-vector<pair<int, int>> a[1001];
-int d[1001], inf = 100000000;
-bool chk[1001];
+vector<pair<int, int>> vec[1001];					// (destination, cost) 저장
+int shortest_dist[1001], inf = 100000000; // 최단거리 저장, 무한대(연결되지 않는 노드는 계속 무한대로, 갱신 되지 않음)
+bool visit_check[1001];										// 디폴트 false, 방문한 곳은 true
 
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < m; i++)
+	int N, M;
+	int start, destination, cost;
+	int goal_start, goal_destination;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++)
 	{
-		// x : from
-		// y : to
-		// z : cost
-		int x, y, z;
-		cin >> x >> y >> z;
-		a[x].push_back(make_pair(y, z));
+		cin >> start >> destination >> cost;
+		a[start].push_back(make_pair(destination, cost));
 	}
-	int start, end;
-	cin >> start >> end;
-	for (int i = 1; i <= n; i++)
+	cin >> goal_start >> goal_destination;
+	for (int i = 1; i <= N; i++)
 	{
-		d[i] = inf;
+		shortest_dist[i] = inf; // 첫 번째 도시부터 N번째 도시를 무한대로 갱신
 	}
-	d[start] = 0;
+	shortest_dist[goal_start] = 0; // 자기 자신으로 가는 가중치는 무조건 0
+
+	// pair.first가 같은 경우 pair.second(cost)가 작은 걸 우선순위로 정렬
+	// 출력할 때 cost가 가장 작은 것을 출력하게 됨
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-	q.push(make_pair(0, start));
+
+	q.push(make_pair(0, goal_start)); // 시작점 가중치 0
 	while (!q.empty())
 	{
-		int node = q.top().second;
+		int curent_node = q.top().second;
 		q.pop();
-		if (!chk[node])
+		if (!visit_check[curent_node]) // 방문하지 않은 노드라면
 		{
-			chk[node] = true;
-			for (int k = 0; k < a[node].size(); k++)
+			visit_check[curent_node] = true; // 방문한 노드 true로 갱신
+			for (int k = 0; k < vec[curent_node].size(); k++)
 			{
-				int next = a[node][k].first;
-				if (d[next] > d[node] + a[node][k].second)
+				int destination_node = vec[curent_node][k].first;
+				// 현재 노드의 최소 비용에서 그 노드에서 목적지 노드로 가는 비용이
+				// 현재 목적지 노드로 가는 비용보다 작다면 갱신
+				if (shortest_dist[destination_node] > shortest_dist[curent_node] + a[curent_node][k].second)
 				{
-					d[next] = d[node] + a[node][k].second;
-					q.push(make_pair(d[next], next));
+					shortest_dist[destination_node] = shortest_dist[curent_node] + a[curent_node][k].second;
+					q.push(make_pair(shortest_dist[destination_node], destination_node));
 				}
 			}
 		}
 	}
-	cout << d[end];
+	// gola_start에서 goal_destination으로 가는 최소 비용 출력
+	cout << shortest_dist[goal_destination];
 }
