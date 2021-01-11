3ccec98fb692ef652ec1213aebedccbcd390d0da
zeze1004 <thwjd2717@naver.com>
2021-01-11 18:18:18 +0900

    10926 해결

---
+++ 10926.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin >> str;
+	cout << str << "??!";
+	return 0;
+}
