29f13c610113c07ce9fed050181fb68e46c41e61
zeze1004 <thwjd2717@naver.com>
2021-01-11 18:25:25 +0900

    11383 해결

---
+++ 11383.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long A, B, C;
+	cin >> A >> B >> C;
+	cout << A + B + C;
+
+	return 0;
+}
