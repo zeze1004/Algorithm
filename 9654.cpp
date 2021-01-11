954995ea9dbec2a951b2a73154ab7bd61a54029a
zeze1004 <thwjd2717@naver.com>
2021-01-11 18:13:40 +0900

    9654 해결

---
+++ 9654.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << '\n'
+			 << "N2 Bomber      Heavy Fighter  Limited    21        " << '\n'
+			 << "J-Type 327     Light Combat   Unlimited  1         " << '\n'
+			 << "NX Cruiser     Medium Fighter Limited    18        " << '\n'
+			 << "N1 Starfighter Medium Fighter Unlimited  25        " << '\n'
+			 << "Royal Cruiser  Light Combat   Limited    4         ";
+	return 0;
+}
