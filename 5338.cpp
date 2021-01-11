03617adc46e29f8eb04cb635656cccc5ea70394c
zeze1004 <thwjd2717@naver.com>
2021-01-11 17:25:03 +0900

    5338 해결

---
+++ 5338.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << "       _.-;;-._" << '\n'
+			 << "'-..-'|   ||   |" << '\n'
+			 << "'-..-'|_.-;;-._|" << '\n'
+			 << "'-..-'|   ||   |" << '\n'
+			 << "'-..-'|_.-''-._|";
+
+	return 0;
+}
