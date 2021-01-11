408afdb85d48785335d9e21ca38434f8d7eea361
zeze1004 <thwjd2717@naver.com>
2021-01-11 17:22:16 +0900

    5337 해결

---
+++ 5337.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << ".  .   ." << '\n'
+			 << "|  | _ | _. _ ._ _  _" << '\n'
+			 << "|/\\|(/.|(_.(_)[ | )(/.";
+
+	return 0;
+}
