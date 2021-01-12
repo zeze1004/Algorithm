b6851654914abf574ca9a87b8b893212363adfa5
zeze1004 <thwjd2717@naver.com>
2021-01-12 20:58:48 +0900

    1654 보류

---
+++ 1654.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int K, N, num[10001];
+	cin >> K >> N;
+	for (int i = 0; i < K; i++)
+	{
+		cin >> num[i];
+	}
+	while (1)
+	{
+	}
+
+	return 0;
+}
