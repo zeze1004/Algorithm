fa16854dab4c25aa1e7290528ec5cd52dd7de2c4
zeze1004 <thwjd2717@naver.com>
2021-01-17 01:49:08 +0900

    다시 풀기

--- 1644.cpp
+++ 1644.cpp
@@ -1,68 +1,64 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <math.h>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	ios_base ::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    // cout.tie(NULL);
 
-	int N;
-	vector<int> prime;
-	cin >> N;
+    int N;
+    vector<int> prime;
+    vector<bool> check;
+    cin >> N;
 
-	// 초기화
-	for (int i = 0; i <= N; i++)
-	{
-		prime.push_back(i);
-	}
+    check.resize(N + 1, true);
+    // 소수판정
+    for (int i = 2; i * i <= N; i++)
+    {
+        if (!check[i])
+            continue;
+        for (int j = i * i; i <= N; j += i)
+        {
+            check[j] = false;
+        }
+    }
 
-	for (int i = 2; i <= sqrt(N); i++)
-	{
-		if (prime[i] == 0)
-			continue;
-		for (int j = i * i; j <= N; j += i)
-		{
-			prime[j] = 0;
-		}
-	}
-	// 출력
-	// for (int i = 2; i <= N; i++)
-	// {
-	// 	if (prime[i] != 0)
-	// 		cout << prime[i] << '\n';
-	// }
-	int num, count, output = 0;
-	// 부르트포스트
-	for (int i = 0; i < N; i++)
-	{
-		num = prime[i];
-		for (int j = i + 1; j < N; j++)
-		{
-			count = 0;
-			// int tmp = j - 1;
-			// 소수면
-			if (prime[i] != 0)
-			{
-				num += prime[j];
-				count++;
-				cout << "Continuous "
-						 << "i: " << i << " j: " << j << " num " << num << " count: " << count << '\n';
-				if (num == N)
-				{
-					output++;
-					cout << "i: " << i << " j: " << j << " num " << num << " count: " << count << '\n';
-				}
-				else if (num > N)
-				{
-					count = 0;
-				}
-			}
-		}
-	}
-	cout << output;
-	return 0;
+    for (int i = 2; i <= N; i++)
+    {
+        if (check[i])
+        {
+            prime.push_back(i);
+            cout << prime[i] << '\n';
+        }
+    }
+
+    // 투 포인터
+    int start = 0, end = 0, count = 0, sum = 0;
+    while (1)
+    {
+        if (sum >= N)
+        {
+            sum -= prime[start++];
+            // start++;
+        }
+        else if (end == prime.size())
+        {
+            break;
+        }
+        else
+        {
+            sum += prime[end++];
+            // end++;
+        }
+        if (sum == N)
+        {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
 }
