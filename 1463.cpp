0d5fd54c999dda88f6dabbd4066924f78b426841
zeze1004 <thwjd2717@naver.com>
2021-01-19 02:03:38 +0900

    DP

--- 1463.cpp
+++ 1463.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	int N, num = 0;
+	int N, arr[1000001];
 	cin >> N;
-	while (1)
+
+	for (int i = 2; i <= N; i++)
 	{
-		if ()
+		arr[i] = arr[i - 1] + 1;
+		if (i % 2 == 0)
+			arr[i] = min(arr[i], arr[i / 2] + 1);
+		if (i % 3 == 0)
+			arr[i] = min(arr[i], arr[i / 3] + 1);
 	}
+
+	cout << arr[N];
+
 	return 0;
 }
