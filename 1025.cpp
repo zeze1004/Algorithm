1d752401dd3d0bdcbf40082c0c29b018204b0203
zeze1004 <thwjd2717@naver.com>
2021-01-24 01:18:24 +0900

    보류

--- 1025.cpp
+++ 1025.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
+#include <string>
 using namespace std;
 
 int main()
@@ -8,16 +10,25 @@ int main()
 	ios_base ::sync_with_stdio(false);
 	cin.tie(NULL);
 	int x, y, input;
-	vector<int> vec;
+	string str;
+	vector<string> vec;
+	vector<int> output;
 	cin >> x >> y;
 	for (int i = 0; i < x; i++)
 	{
 		for (int j = 0; j < y; j++)
 		{
-			cin >> input;
-			vec.push_back(input);
+			cin >> str;
+			vec.push_back(str);
 			// 입력 받는 모든 수는 등차수열인 것
-				}
+		}
+	}
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+		}
 	}
 
 	return 0;
