f39ad4a2b98827284cfc33c0f4bbfb5ff2417c00
zeze1004 <thwjd2717@naver.com>
2021-01-28 13:15:20 +0900

    12101

---
+++ 12101.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define max 987654321
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	// 흠 이걸 어케하지
+	// 완전 탐색 결과를 배열에 저장...
+	// 원소 하나하나 저장?
+	// 흠
+	long long num, arr[max];
+	vector<int> vec;
+	cin >> num;
+
+	while (1)
+	{
+	}
+
+	return 0;
+}
