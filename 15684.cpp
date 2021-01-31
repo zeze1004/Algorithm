6d8b82690b0b56544b6d88ee8385d4a29ccadaeb
zeze1004 <thwjd2717@naver.com>
2021-01-31 14:33:08 +0900

    보류

---
+++ 15684.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N, M, H;
+
+	return 0;
+}
