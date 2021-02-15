55ac44dae24a016c3e3112a948251cabbd7ea08b
zeze1004 <thwjd2717@naver.com>
2021-02-15 18:01:12 +0900

    주석 추가

--- 2798.py
+++ 2798.py
@@ -7,7 +7,8 @@ for i in range(N):
         for k in range(j+1, N):
             if card_num[i]+card_num[j]+card_num[k] > M:
                 continue
+                # M이 초과하지 않은 카드의 합은 무조건 ouput값과 비교해서 ouput보다 크면 ouput 값 갱신
             else:
                 output = max(output, card_num[i]+card_num[j]+card_num[k])
-
+# 전체 카드 숫자 비교가 끝나면 ouput 값 출력
 print(output)
