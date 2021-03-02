6149ea192af5e8359e9dad0548ce286b75ec12ee
zeze1004 <thwjd2717@naver.com>
2021-03-03 00:51:02 +0900

    CCW 알고리즘

---
+++ 11758.py
@@ -0,0 +1,12 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+
+result = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
+
+if result == 0:
+    print(0)
+elif result > 0:
+    print(1)
+else:
+    print(-1)
