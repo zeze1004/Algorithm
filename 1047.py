4537e3db75fee1949fed3346dbc225afee7a69ad
zeze1004 <thwjd2717@naver.com>
2021-03-04 01:40:45 +0900

    다시풀기

--- 1047.py
+++ 1047.py
@@ -19,3 +19,30 @@ subin_place, sister_place = map(int, input().split())
 time = [0] * 100001
 
 bfs()
+
+# https://velog.io/@devjuun_s/%EC%88%A8%EB%B0%94%EA%BC%AD%EC%A7%88-%EB%B0%B1%EC%A4%80-1697%EB%B2%88python
+# from collections import deque
+
+# def bfs(v):
+#     count = 0
+#     q = deque([[v, count]])
+#     while q:
+#         v = q.popleft()
+#         e = v[0]
+#         count = v[1]
+#         if not visited[e]:
+#             visited[e] = True
+#             if e == K:
+#                 return count
+#             count += 1
+#             if (e * 2) <= 100000:
+#                 q.append([e * 2, count])
+#             if (e + 1) <= 100000:
+#                 q.append([e + 1, count])
+#             if (e - 1) >= 0:
+#                 q.append([e - 1, count])
+#     return count
+
+# N, K = map(int, input().split())
+# visited = [False] * 100001
+# print(bfs(N))
