2e753947852b326cffc2a4198a8c4c889d21bfb1
zeze1004 <thwjd2717@naver.com>
2021-03-09 00:08:54 +0900

    이분탐색

--- 10815.py
+++ 10815.py
@@ -1,9 +1,24 @@
 card_num = int(input())
 
 have_card = list(map(int, input().split()))
+have_card.sort()
 
 question_card_num = int(input())
 
 question_card = list(map(int, input().split()))
 
-result = []
+for i in question_card:
+    left, right, result = 0, card_num - 1, False
+    while left <= right:
+        mid = (left + right) // 2
+        if have_card[mid] == i:
+            result = True
+            break
+        elif have_card[mid] < i:
+            left = mid + 1
+        else:
+            right = mid - 1
+    if result:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
