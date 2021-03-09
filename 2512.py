07578ec2dcd2ef82ef8394f6362363f60cbe849f
zeze1004 <thwjd2717@naver.com>
2021-03-09 14:29:27 +0900

    뭉기 코드리뷰

--- 2512.py
+++ 2512.py
@@ -4,28 +4,25 @@ budget = list(map(int, input().split()))
 
 max_budget = int(input())
 
-left, right = 0, max(budget)
+# 1. 모든 요청이 배정될 수 있는 경우에는 요청한 금액을 그대로 배정한다.
+if sum(budget) <= max_budget:
+    print(max(budget))
 
-while left + 1 < right:
-    sum_budget = 0
-    mid = (left + right) // 2
-    # 작은 것 부터 더해서 최댓값 구한다
-    for i in budget:
-        sum_budget += min(i, mid)
-
-    if sum_budget <= max_budget:
-        left = mid
-    else:
-        right = mid
-
-total_budget = 0
-for i in budget:
-    if i < right:
-        total_budget += i
-    else:
-        total_budget += right
-
-if total_budget > max_budget:
-    print(right-1)
+# 2. 그렇지 않으면 특정한 정수 상한액을 계산하여 그 이상인 예산요청에는 모두 상한액을 배정한다.
 else:
-    print(right)
+    # left : success / right : fail
+    left, right = 0, max(budget)
+
+    while left + 1 < right:
+        sum_budget = 0
+        mid = (left + right) // 2
+        # 작은 것 부터 더해서 최댓값 구한다
+        for i in budget:
+            sum_budget += min(i, mid)
+
+        if sum_budget <= max_budget:
+            left = mid
+        else:
+            right = mid
+    # print max success
+    print(left)
