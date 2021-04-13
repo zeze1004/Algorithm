27155301ddee5ce772102032957ca30796525ce0
zeze1004 <thwjd2717@naver.com>
2021-04-13 19:51:22 +0900

    알림 그리디 연습문제

---
+++ backjoon/133050.py
@@ -0,0 +1,15 @@
+city_num = int(input())
+road_length = list(map(int, input().split()))
+price_gas = list(map(int, input().split()))
+
+# 처음 도시에서 다음 도시의 길이만큼 꼭 주유 해야함
+min_money = price_gas[0]
+total_money = 0
+
+# 제일 작은 주유값이 나타나면 갱신하고 도로 길이만큼 제일 작은 주유값 곱함 
+for i in range(city_num - 1):
+    if price_gas[i] < min_money:
+        min_money = price_gas[i]
+    total_money += min_money * road_length[i]
+
+print(total_money)
