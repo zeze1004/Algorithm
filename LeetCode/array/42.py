978c6dfad02be7519d2bc9a3086bf366c092b739
zeze1004 <thwjd2717@naver.com>
2021-05-11 02:34:33 +0900

    스택 풀이 추가하기

--- LeetCode/array/42.py
+++ LeetCode/array/42.py
@@ -1,5 +1,7 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    # 투포인터
+    # 최대 지점에서 좌우 포인트가 서로 만나게 되며 O(n) 풀이 가능
+    def trap1(self, height: List[int]) -> int:
         if len(height) == 0:
             return 0
 
@@ -9,13 +11,12 @@ class Solution:
         left_max, right_max = height[left], height[right]
         while left < right:
 
-            left_max, right_max = max(height[left], left_max), max(
-                height[right], right_max)
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
             print('left_max: ', left_max)
             print('right_max: ', right_max)
 
             # 더 높은 쪽을 향해 포인터 이동
-            # 최대 지점에서 좌우 포인터가 서로 만나면서 종료
+            # 가장 높은 중간 지점에서 포인터 멈춤
             if left_max < right_max:
                 water += left_max - height[left]
                 left += 1
