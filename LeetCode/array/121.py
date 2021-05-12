8e8246512e14acc7633f99a21fbd1e5e0e0922df
zeze1004 <thwjd2717@naver.com>
2021-05-13 02:02:54 +0900

    리트코드 업로드

--- LeetCode/array/121.py
+++ LeetCode/array/121.py
@@ -1,7 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_num = max(prices)
+        min_num = max(prices) # sys.maxsize
         result = 0
+        # 최솟값과 최댓값을 계속 갱신
         for i in prices:
             min_num = min(min_num, i)
             result = max(i - min_num, result)
--- LeetCode/stack,que/20.py
+++ LeetCode/stack,que/20.py
@@ -8,9 +8,11 @@ class Solution:
         }
 
         for i in s:
-            if i not in table:
+            if i not in table: # (, {, [
                 stack.append(i)
             # 스택이 비어있지 않거나 테이블의 키값과 pop 값이 value로 일치하지 않으면 false
             elif not stack or table[i] != stack.pop():
                 return False
+        # input이 '[' 일 때 stack에 append하고 반복문 종료
+        # 바로 return True를 하면 틀리므로 마지막까지 체크
         return len(stack) == 0
