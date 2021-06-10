341effb14cc3d1aaa0c821f7b695b0f3a1567cb5
zeze1004 <thwjd2717@naver.com>
2021-06-10 15:16:30 +0900

    DP 복습

---
+++ backjoon/11727_3.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+tile = [1, 3]
+
+num = int(input())
+
+for i in range(2, num):
+    tile.append(tile[i - 1] + (tile[i - 2] * 2))
+
+print(tile[num - 1] % 10007)
