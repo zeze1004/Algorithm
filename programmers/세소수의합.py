0ea5e24f698bc401294b84634de368e28de7042a
zeze <thwjd2717@gmail.com>
2022-12-11 15:43:48 +0900

    refact: 보기 쉽게 수정 및 주석 추가

--- programmers/세소수의합.py
+++ programmers/세소수의합.py
@@ -1,14 +1,20 @@
-import math
 from itertools import combinations
+
+
 def solution(n):
-    check = [False, False] + [True]*(n-1)
-    for i in range(2, math.ceil(n**0.5)):
-        for j in range(i*2, n+1, i):
-            check[j] = False
-    pn = [i for i in range(2,n) if check[i] == True]
-    comb = combinations(pn, 3)
-    cnt = 0
-    for c in comb:
-        if sum(c) == n:
-            cnt += 1
-    return cnt
+    ans = 0
+    prime_array = [True] * n
+    minimum = int(n ** 0.5)  # n의 제곱근 수까지만 배수를 구하면 시간절약 가능한
+
+    for i in range(2, minimum + 1):
+        if prime_array[i] == True:
+            for j in range(i + i, n, i):
+                prime_array[j] = False
+
+    prime_num = [i for i in range(2, n) if prime_array[i] == True]  # 2 ~ n 중에서 소수 리스트
+    comb = combinations(prime_num, 3)  # 중복 없이 랜덤 3가지 조합
+    for i in comb:
+        if sum(i) == n:
+            ans += 1
+
+    return ans
