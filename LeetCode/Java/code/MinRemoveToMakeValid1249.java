76769519672c01f22881172b60900d437de4d2d8
zeze <thwjd2717@gmail.com>
2023-10-29 14:37:38 +0900

    add: leetcode 1249 스택 문제 풀이 추가

---
+++ LeetCode/Java/code/MinRemoveToMakeValid1249.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+import java.lang.String
+
+public class MinRemoveToMakeValid1249 {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (!stack.empty() && s.charAt(i) == ')') {
+                charArray[i] = '!';             // '(' == '!'
+                charArray[stack.pop()] = '?';   // ')' == '?'
+            }
+        }
+        s = String.valueOf(charArray);
+
+        s = s.replace("(", "");
+        s = s.replace(")", "");
+        s = s.replace("?", "(");
+        s = s.replace("!", ")");
+
+        return s;
+    }
+}
