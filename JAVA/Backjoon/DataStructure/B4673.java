a806c18338bfa11187a656225176c1e857599789
zeze <thwjd2717@gmail.com>
2024-03-06 11:01:37 +0900

    refactor: 10을 나눠서 자릿수 별로 더하는 풀이 추가

--- JAVA/Backjoon/DataStructure/B4673.java
+++ JAVA/Backjoon/DataStructure/B4673.java
@@ -6,19 +6,40 @@ public class B4673 {
         TreeSet<Integer> unSelfNum = new TreeSet<>();
 
         for (int i = 1; i <= 10000; i++) {
-            String numS = String.valueOf(i);
-            String[] numSplit = numS.split("");
-
-            int sumN = i;
-            for (String s : numSplit) {
-                sumN += Integer.parseInt(s);
-            }
-            unSelfNum.add(sumN);
+            unSelfNum.add(splitAndSumDigit(i));
         }
 
         for (int i = 1; i <= 10000; i++) {
             if (!unSelfNum.contains(i))
                 System.out.println(i);
         }
+
     }
+
+    // 반복문으로 수를 10으로 계속 나누워 모든 자릿수의 값을 하나 씩 더하는 풀이
+    public static int splitAndSumDigit(int num) {
+        int sum  = num;
+
+        while (num != 0) {
+            sum += num % 10; // 일의 자리 수 더하기
+            num /= 10;       // 기존 일의 자리 수를 제거하여 앞의 수가 일의 자리로 옴
+        }
+
+        return sum;
+    }
+
+    // 숫자를 문자로 받아서 문자열 split 후 전체 합하는 방식의 풀이
+    public static int SplitStr(int num) {
+
+        String numS = String.valueOf(num);
+        String[] numSplit = numS.split("");
+
+        int sum = num;
+        for (String s : numSplit) {
+            sum += Integer.parseInt(s);
+        }
+
+        return sum;
+    }
+
 }
