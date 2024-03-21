0d8913967aac132dc38f583799833041bd2d80c5
sojung.kim <thwjd2717@gmail.com>
2024-03-21 09:57:59 +0900

    백준: 에라토스테네스의 체 구현

---
+++ JAVA/Backjoon/DataStructure/B2960.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2960 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean[] check = new boolean[N + 1];
+        int count = 0;
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = i; j <= N; j += i) { // i의 배수 간격으로 반복
+                if (!check[j]) {
+                    check[j] = true;
+                    count++;
+                    if (count == K) {
+                        System.out.println(j);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
