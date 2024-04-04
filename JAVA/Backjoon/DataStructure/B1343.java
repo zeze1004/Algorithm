35c6684ca8dded22c84fd10b248fb27d479c04ea
sojung.kim <thwjd2717@gmail.com>
2024-04-04 09:35:52 +0900

    백준: 폴리오미노 문제 풀이

---
+++ JAVA/Backjoon/DataStructure/B1343.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1343 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder board = new StringBuilder(br.readLine());
+        StringBuilder ans = new StringBuilder();
+
+        int count = 0;
+        for (int i = 0; i < board.length();) {
+            if (board.charAt(i) == '.') {
+                count = 0;
+                ans.append('.');
+                i++;
+            } else {
+                while (i < board.length() && board.charAt(i) == 'X') {
+                    count++;
+                    i++;
+                }
+                if (count == 2) {
+                    ans.append("BB");
+                } else if (count == 4) {
+                    ans.append("AAAA");
+                } else if (count % 4 == 2) {
+                    int tmp = count / 4;
+                    for (int j = 4; j < count; j+=4) {
+                        ans.append("AAAA");
+                    }
+                    ans.append("BB");
+                } else if (count % 4 == 0) {
+                    int tmp = count / 4;
+                    for (int j = 0; j < tmp; j++) {
+                        ans.append("AAAA");
+                    }
+                } else {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
