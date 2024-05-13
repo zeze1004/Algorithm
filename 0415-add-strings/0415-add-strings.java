9f6826a308ec19246f2ad9385db8aa13cd5a3fab
sojung.kim <thwjd2717@gmail.com>
2024-05-13 09:31:33 +0900

    Time: 1 ms (100%), Space: 42.2 MB (81.52%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,9 +1,18 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        Long a = Long.parseLong(num1); Long b = Long.parseLong(num2);
-        System.out.println(a + " " + b);
-        String output = Long.toString(a + b);
-        return output;
-        // return output.substring(0, output.length() - 2);
+        StringBuilder output = new StringBuilder();
+        int carry = 0;
+        int len1 = num1.length() - 1, len2 = num2.length() - 1;
+
+        while (len1 >= 0 || len2 >= 0 || carry != 0) {
+            int x1 = len1 >= 0 ? num1.charAt(len1) - '0' : 0;
+            int x2 = len2 >= 0 ? num2.charAt(len2) - '0' : 0;
+            int value = (x1 + x2 + carry) % 10; // 더할 일의자리 수
+            carry = (x1 + x2 + carry) / 10; // 승수
+            output.append(value);
+            len1--; len2--;
+        }
+
+        return output.reverse().toString();
     }
 }
