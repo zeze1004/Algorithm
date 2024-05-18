c47a1876a60880ccd44e82333b714fcb00b69f73
sojung.kim <thwjd2717@gmail.com>
2024-05-18 18:04:08 +0900

    Time: 48 ms (40.3%), Space: 17.2 MB (29.11%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -10,7 +10,8 @@ class Solution:
             return None
         root.left = self.removeLeafNodes(root.left, target)
         root.right = self.removeLeafNodes(root.right, target)
+
         if root.val == target and root.left == None and root.right == None:
             return None
-        return root
         
+        return root
