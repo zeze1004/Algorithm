bf1672b039518eae1cb2afc46d22093da6738b19
sojung.kim <thwjd2717@gmail.com>
2024-05-18 13:32:27 +0900

    Time: 0 ms (100%), Space: 41.6 MB (93.06%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.java
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.java
@@ -24,30 +24,20 @@ class Solution {
     public void dfs(TreeNode root, TreeNode parent) {
         if (root == null) return;
 
-        System.out.printf("root.val: %d\n", root.val);
-
+        // 현재의 노드가 부모노드로 전달
         dfs(root.left, root);
         dfs(root.right, root);
         
+        // 부모의 코인이 자식에게 이동해야함
+        // 이동하는 코인의 갯수가 곧 이동 횟수
         if (root.val < 1) {
-            System.out.printf("root.val < 1\n");
-            System.out.printf("root.val: %d\n", root.val);
-            System.out.printf("parent.val: %d\n", parent.val);
             output += (1 - root.val);
             parent.val -= (1 - root.val); 
-            System.out.printf("After\n");
-            System.out.printf("root.val: %d\n", root.val);
-            System.out.printf("parent.val: %d\n", parent.val);
         }
+        // 현재 노드의 코인이 1보다 많은 경우, 1 - root.val 코인을 부모에게 이동
         else if(root.val > 1) {
-            System.out.printf("root.val > 1\n");
-            System.out.printf("root.val: %d\n", root.val);
-            System.out.printf("parent.val: %d\n", parent.val);
             output += root.val - 1;
             parent.val += root.val - 1; 
-            System.out.printf("After\n");
-            System.out.printf("root.val: %d\n", root.val);
-            System.out.printf("parent.val: %d\n", parent.val);
         }
     }
 }
