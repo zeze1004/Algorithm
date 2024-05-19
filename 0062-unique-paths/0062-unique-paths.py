1102b88939bd3b0c4e53d62975e0a51da3c7b507
sojung.kim <thwjd2717@gmail.com>
2024-05-19 19:02:55 +0900

    Time: 27 ms (94.72%), Space: 16.5 MB (86.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,19 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # DP 배열 초기화
         dp = [[0] * n for _ in range(m)]
-
-        # 0행과 0열을 1로 초기화
-        for i in range(n):
-            dp[0][i] = 1
-        for i in range(m):
-            dp[i][0] = 1
         
-        # 바로 위에서 내려온 경로의 총 경우의 수와 왼쪽에서 오른쪽으로 오는 총 경우의 수를 더한다
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        # 0행, 0열을 일직선으로 쭉 가는 경로의 경우의 수는 1
+        for y in range(n):
+            dp[0][y] = 1
+        for x in range(m):
+            dp[x][0] = 1
+        
+        for x in range(1, m):
+            for y in range(1, n):
+                dp[x][y] = dp[x - 1][y] + dp[x][y - 1]
         
         return dp[m - 1][n - 1]
-        
-        
+   
