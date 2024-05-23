991e5f12db745e63d670620b6f1b0b2a8ab3ca26
sojung.kim <thwjd2717@gmail.com>
2024-05-23 11:37:21 +0900

    Time: 7 ms (96.56%), Space: 56.9 MB (46.13%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,36 +1,35 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        // Edge case
-        if(s == null || s.length() == 0) return new ArrayList<>();
+        List<List<String>> output = new ArrayList<>();
         
-        List<List<String>> result = new ArrayList<>();
-        helper(s, new ArrayList<>(), result);
-        return result;
+        backtrack(s, output, new ArrayList<>(), 0);
+        return output;
     }
-    public void helper(String s, List<String> step, List<List<String>> result) {
-        // Base case
-        if(s == null || s.length() == 0) {
-            result.add(new ArrayList<>(step));
+
+    public void backtrack(String s, List<List<String>> output, List<String> sub, int idx) {
+        if (idx == s.length()) {
+            output.add(new ArrayList<>(sub));
             return;
         }
-        for(int i = 1; i <= s.length(); i++) {
-            String temp = s.substring(0, i);
-            if(!isPalindrome(temp)) continue; // only do backtracking when current string is palindrome
-            
-            step.add(temp);  // choose
-            helper(s.substring(i, s.length()), step, result); // explore
-            step.remove(step.size() - 1); // unchoose
+
+        for (int i = idx; i < s.length(); i++) {
+            String tmp = s.substring(idx, i + 1);
+            if (isPalindrom(tmp)) {
+                sub.add(tmp);
+                backtrack(s, output, sub, i + 1);
+                sub.remove(sub.size() - 1); // 이전 과정으로 돌아가기 위해. 제거하지 않으면 중복값이 생김.
+            }
         }
-        return;
     }
-    public boolean isPalindrome(String s) {
+
+    public boolean isPalindrom(String s) {
         int left = 0, right = s.length() - 1;
-        while(left <= right) {
-            if(s.charAt(left) != s.charAt(right))
-                return false;
-            left ++;
-            right --;
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
         }
+
         return true;
     }
 }
