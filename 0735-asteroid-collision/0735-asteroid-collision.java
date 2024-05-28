12800a102a2874c18cd5ef53c31e054e48a7d449
sojung.kim <thwjd2717@gmail.com>
2024-05-28 16:18:06 +0900

    Time: 5 ms (84.28%), Space: 45.2 MB (62.12%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int len = asteroids.length, idx = 0;
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < len; i++) {
-            int compare = asteroids[i];
-            while (idx > 0 && asteroids[idx - 1] > 0 && compare < 0 && asteroids[idx - 1] < -compare) idx--;
-            
-            if (idx == 0 || compare > 0 || asteroids[idx - 1] < 0) {
-                asteroids[idx++] = compare;
-            } else if (asteroids[idx - 1] == -compare) {
-                idx--;
+        main: for (int i = 0; i < asteroids.length; i++) {
+            int a = asteroids[i];
+            boolean canPush = true;
+    
+            if (a > 0) stack.push(a);
+            else {
+                while (!stack.isEmpty()) {
+                    if (stack.peek() > 0 && stack.peek() < -a) stack.pop();
+                    else if (stack.peek() > 0 && stack.peek() > -a) {
+                        canPush = false;
+                        break;
+                    }
+                    else if (stack.peek() == -a) {
+                        stack.pop();
+                        canPush = false;
+                        break;
+                    } 
+                    else if (stack.peek() < 0) {
+                        stack.push(a);
+                        canPush = false;
+                        break;
+                    }
+                }
+
+                if (canPush) stack.push(a);
             }
         }
 
-        int[] output = new int[idx];
-        int start = 0, end = idx;
-        System.arraycopy(asteroids, start, output, start, end);
+        int[] output = new int[stack.size()];
+        
+        for (int i = output.length - 1; i >= 0; i--) {
+            output[i] = stack.pop();
+        }
 
         return output;
     }
