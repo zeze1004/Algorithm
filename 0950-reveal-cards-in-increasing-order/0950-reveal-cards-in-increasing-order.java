bbd8d08daae68e7b404b3e7a593dbfcafa1879e0
sojung.kim <thwjd2717@gmail.com>
2024-06-11 09:44:23 +0900

    Time: 5 ms (15.92%), Space: 44.6 MB (6%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        // 덱을 오름차순으로 정렬
+        Arrays.sort(deck);
+        
+        // 결과를 저장할 큐를 생성
+        LinkedList<Integer> resultQueue = new LinkedList<>();
+        
+        // 가장 큰 카드부터 작은 카드 순서로 배치
+        for (int i = deck.length - 1; i >= 0; i--) {
+            // System.out.println(resultQueue);
+            if (!resultQueue.isEmpty()) {
+                int last = resultQueue.removeLast();
+                // System.out.printf("que's last: %d\n", last);
+                // 큐의 마지막 요소를 큐의 맨 앞으로 이동
+                resultQueue.addFirst(last);
+            }
+            // 현재 카드를 큐의 맨 앞에 추가
+            // System.out.printf("que's first: %d\n", deck[i]);
+            resultQueue.addFirst(deck[i]);
+        }
+        
+        // 결과 큐를 배열로 변환
+        int[] result = new int[deck.length];
+        for (int i = 0; i < deck.length; i++) {
+            result[i] = resultQueue.removeFirst();
+        }
+        
+        return result;
+    }
+}
