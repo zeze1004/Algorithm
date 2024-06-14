ec83e832d9c2878f8b6b14aca0780a7b57906774
sojung.kim <thwjd2717@gmail.com>
2024-06-14 13:40:18 +0900

    Time: 1 ms (94.37%), Space: 44.5 MB (16.31%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Deque<Integer> stack = new ArrayDeque<>();
+        Deque<Integer> deque = new LinkedList<>();
         int idx = 0;
-
-        for (int i : pushed) {
-            stack.push(i);
-            // System.out.printf("i: %d\n", i);
-            while(!stack.isEmpty() && stack.peek() == popped[idx]) {
-                // System.out.printf("stack.peek(): %d, popped[%d]: %d\n", stack.peek(), idx, popped[idx]);
-                stack.pop();
+        for (Integer i : pushed) {
+            deque.push(i);
+            while (!deque.isEmpty() && deque.peek() == popped[idx]) {
+                deque.pop();
                 idx++;
             }
         }
-        if (stack.isEmpty()) return true;
-        return false;
+
+        return deque.isEmpty() ? true : false;
     }
 }
