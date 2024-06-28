47cda3b0c794e7b7969ee66a276f3099d3cfccb1
sojung.kim <thwjd2717@gmail.com>
2024-06-28 19:40:49 +0900

    Time: 6 ms (39.53%), Space: 41.7 MB (38.73%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.java
+++ 1006-clumsy-factorial/1006-clumsy-factorial.java
@@ -1,38 +1,39 @@
 class Solution {
     public int clumsy(int n) {
-        int output = n, sign = 0;
-        int idx =  n / 4; int t = 0; int plus = 0;
-        List<Integer> tmp = new ArrayList<>();
-        for (int i = 0; i < n; i+=4) { // i = 0, 4, 8
+        List<Integer> tmpList = new ArrayList<>();
+        int output = n;
+        int idx =  n / 4; 
+        int tmpI = 0;
+        int plus = 0;
+        
+        for (int i = 0; i < n; i+=4) {      // i = 0, 4, 8
             if (idx == 0) {
-                t = i;
+                tmpI = i;
                 break;
             }
+
             output = (n - i) * (n - i - 1); // (10 * 9), (6 * 5)
-            output = output / (n - i - 2); // (output / 8), (output / 4)
-            plus += n - i - 3;            // (output + 7), (ouptut + 3)
-            System.out.printf("output: %d\n", output);
-            tmp.add(output);
+            output = output / (n - i - 2);  // (output / 8), (output / 4)
+            plus += n - i - 3;              // (output + 7), (ouptut + 3)
+
+            tmpList.add(output);
             idx--;
         }
 
-        if (n % 4 != 0) { // idx = 1, 2, 3 {
-            if (n % 4 == 1 || n % 4 == 2) tmp.add(n - t);
+        if (n % 4 != 0) {  
+            if (n % 4 == 1 || n % 4 == 2) tmpList.add(n - tmpI);
             else if (n % 4 == 3) {
-                output = (n - t) * (n - t - 1);
-                output = output / (n - t - 2);
-                tmp.add(output);
+                output = (n - tmpI) * (n - tmpI - 1);
+                output = output / (n - tmpI - 2);
+                tmpList.add(output);
             }
         }
-        
 
-
-        int output2 = tmp.get(0);
-        for (int i = 1; i < tmp.size(); i++) {
-            System.out.println(output2);
-            output2 -= tmp.get(i);
+        int rns = tmpList.get(0);
+        for (int i = 1; i < tmpList.size(); i++) {
+            rns -= tmpList.get(i);
         }
 
-        return output2 + plus;
+        return rns + plus;
     }
 }
