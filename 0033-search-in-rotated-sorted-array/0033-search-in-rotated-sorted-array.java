2daf2e53b04199c4c8151006fdf16fd4fd7cf133
sojung.kim <thwjd2717@gmail.com>
2024-10-20 15:51:36 +0900

    Time: 0 ms (100%), Space: 41.9 MB (55.32%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        // O(log n): 바이너리 서치 사용
+        int left = 0, right = nums.length - 1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
-
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] >= nums[left]) {
-                if (nums[left] <= target && target <= nums[mid]) {
+            }
+
+            // 정렬된 범위 구분
+            // left ~ mid의 범위가 정렬된 상태
+            if (nums[left] <= nums[mid]) {
+                // 왼쪽 범위에 target이 있는지 확인
+                if (nums[mid] >= target && nums[left] <= target) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
-                if (nums[mid] <= target && target <= nums[right]) {
+            } 
+            // mid ~ right의 범위가 정렬된 상태
+            else if (nums[right] >= nums[mid]) {
+                if (nums[mid] <= target && nums[right] >= target) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
