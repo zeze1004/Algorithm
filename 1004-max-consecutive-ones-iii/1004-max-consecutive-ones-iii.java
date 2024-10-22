ce459eab276554008e9eee30bf644df6ce8a5d87
sojung.kim <thwjd2717@gmail.com>
2024-10-22 11:02:29 +0900

    Time: 3 ms (87.79%), Space: 46.7 MB (78.62%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,24 +1,26 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int start = 0;
-        int zeroNum = 0;
+        int zero = 0;
         int output = 0;
-
+        
         for (int end = 0; end < nums.length; end++) {
             if (nums[end] == 0) {
-                zeroNum++;
-                while (zeroNum > k) {
-                    if (nums[start] != 0) {
+                zero++;
+                while (zero > k) {
+                    if (nums[start] == 0) {
+                        zero--;
                         start++;
                     } else {
-                        zeroNum--;
                         start++;
                     }
                 }
             }
-            output = Math.max(output, end - start + 1); // end - start + 1을 해야 해당 인덱스 범위의 원소 개수임
+
+            output = Math.max(output, end - start + 1);
         }
-        return output;    
+
+        return output;
     }
 }
         
