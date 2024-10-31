b5198080fd1bbf1cdfa8562744bacee7459ed918
sojung.kim <thwjd2717@gmail.com>
2024-10-31 09:12:25 +0900

    Time: 0 ms (100%), Space: 40.7 MB (91.6%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,12 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        else if (p == null || q == null) return false;        
-        if (p.val != q.val) return false;
-        isSameTree(p.left, q.left);
-        isSameTree(p.right, q.right);
+        return isMirror(p, q);
+    }
 
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+
+        return (t1.val == t2.val) && isMirror(t1.left, t2.left) && isMirror(t1.right, t2.right);
     }
 }
