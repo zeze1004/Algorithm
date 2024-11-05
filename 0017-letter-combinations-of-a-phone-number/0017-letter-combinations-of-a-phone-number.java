401ed2f444b05251acec8be97a30b163e955652f
sojung.kim <thwjd2717@gmail.com>
2024-11-05 16:44:11 +0900

    Time: 1 ms (57.49%), Space: 42.3 MB (18.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -16,24 +16,22 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> output = new ArrayList<>();
         if (digits.length() == 0) return output;
-        backtracking(output, new StringBuilder(), digits, 0);
+    
+        backtracking(digits, output, 0, new StringBuilder());
 
         return output;
     }
 
-    private void backtracking(List<String> output, StringBuilder sb, String digits, int index) {
-        // 종료 조건
-        if (index == digits.length()) {
-            output.add(sb.toString());
+    private void backtracking(String digits, List<String> output, int index, StringBuilder sb) {
+        if (index >= digits.length()) {
+            output.add(new StringBuilder(sb).toString());
             return;
         }
-
-        String letters = KEYS[digits.charAt(index) - '0'];
-        for (int i = 0; i < letters.length(); i++) {
-            sb.append(letters.charAt(i));
-            // 다음 문자를 추가하기 위해 재귀 호출
-            backtracking(output, sb, digits, index + 1);
-            // 추가된 문자는 제거
+ 
+        String nums = KEYS[digits.charAt(index) - '0'];
+        for (int i = 0; i < nums.length(); i++) {
+            sb.append(nums.charAt(i));
+            backtracking(digits, output, index + 1, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
