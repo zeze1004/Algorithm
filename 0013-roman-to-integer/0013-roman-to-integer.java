1666693b762eeb0c79dfc3f4437491fa7d71205f
sojung.kim <thwjd2717@gmail.com>
2025-02-24 23:51:27 +0900

    Time: 4 ms (68.01%), Space: 44.8 MB (51.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,32 +1,29 @@
-import java.util.*;
-
 class Solution {
     public int romanToInt(String s) {
-        Hashtable<Character, Integer> ht = new Hashtable<>();
-        int output = 0;
-
-        ht.put('I', 1);
-        ht.put('V', 5);
-        ht.put('X', 10);
-        ht.put('L', 50);
-        ht.put('C', 100);
-        ht.put('D', 500);
-        ht.put('M', 1000);
-
-        if (s.length() == 1)
-            return ht.get(s.charAt(0));
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (ht.get(s.charAt(i)) < ht.get(s.charAt(i + 1))) {
-                output += ht.get(s.charAt(i + 1)) - ht.get(s.charAt(i));
-                i++;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int total = 0;
+        int prev = 0; // 이전 문자 값
+        
+        // 오른쪽에서 왼쪽으로 처리
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = map.get(s.charAt(i));
+            
+            if (curr < prev) {
+                total -= curr; // 작은 값이 앞에 있으면 빼줌
+            } else {
+                total += curr;
             }
-            else output += ht.get(s.charAt(i));
+            prev = curr; // 현재 값을 이전 값으로 업데이트
         }
         
-        if (ht.get(s.charAt(s.length() - 2)) >= ht.get(s.charAt(s.length() - 1)))
-            output += ht.get(s.charAt(s.length() - 1));
-        
-        return output;
+        return total;
     }
 }
