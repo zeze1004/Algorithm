875687983b775a76c4a59bf617d83cf0b77537fd
sojung.kim <thwjd2717@gmail.com>
2025-02-25 21:21:00 +0900

    Time: 1 ms (99.9%), Space: 44.6 MB (80.74%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> output = new ArrayList<>(); 
+        backtrack(output, new ArrayList<Integer>(), candidates, target, 0);
+
+        return output;
+    }
+
+    private void backtrack(List<List<Integer>> output, List<Integer> tmp, int[] candidates, int target, int idx) {
+        if (target == 0) {
+            output.add(new ArrayList<>(tmp)); // 깊은 복사
+            return;
+        }
+
+        // 재귀
+        for (int i = idx; i < candidates.length; i++) {
+            if (target < candidates[i]) continue; // 가지치기
+
+            tmp.add(candidates[i]);
+            backtrack(output, tmp, candidates, target - candidates[i], i); // 같은 숫자 중복 사용 가능하므로 동일한 idx 전달
+            tmp.remove(tmp.size() - 1); // 백트래킹(마지막으로 추가한 요소 제거)
+        }
+    }
+}
