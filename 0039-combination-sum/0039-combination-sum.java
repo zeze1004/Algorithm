3614f6b25fca3d7cb39e2fcdbf957c2930d0b042
sojung.kim <thwjd2717@gmail.com>
2025-02-26 21:13:59 +0900

    Time: 2 ms (83.14%), Space: 44.7 MB (63.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> output = new ArrayList<>(); 
+        List<List<Integer>> output = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(output, new ArrayList<Integer>(), candidates, target, 0);
+        backtracking(candidates, target, output, new ArrayList<>(), 0);
 
         return output;
     }
 
-    private void backtrack(List<List<Integer>> output, List<Integer> tmp, int[] candidates, int target, int idx) {
+    private void backtracking(int[] candidates, int target, List<List<Integer>> output, List<Integer> current, int idx) {
         if (target == 0) {
-            output.add(new ArrayList<>(tmp)); // 깊은 복사
+            output.add(new ArrayList<>(current)); // 깊은복사
             return;
         }
 
-        // 재귀
         for (int i = idx; i < candidates.length; i++) {
-            if (target < candidates[i]) return; // 가지치기: 개별 원소가 target 보다 크면 값이 될 수 없기에 종료
-
-            tmp.add(candidates[i]);
-            backtrack(output, tmp, candidates, target - candidates[i], i); // 같은 숫자 중복 사용 가능하므로 동일한 idx 전달
-            tmp.remove(tmp.size() - 1); // 백트래킹(마지막으로 추가한 요소 제거)
+            if (target < candidates[i]) return;
+            current.add(candidates[i]);
+            backtracking(candidates, target - candidates[i], output, current, i);
+            current.remove(current.size() - 1);
         }
     }
 }
